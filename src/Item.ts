4bbc30e6b834b036ec98eb5e3eb484d551f2805a
Ivaylo Ushev <iushev@yahoo.com>
2023-03-16 08:29:46 +0200

    Add data field to rbac item

--- src/Item.ts
+++ src/Item.ts
@@ -8,21 +8,24 @@ export interface IItem {
   name: string;
   description: string | null;
   ruleName: string | null;
+  data: string | null;
 }
 
-export type ICreateItem = Omit<Item, "type" | "description" | "ruleName"> &
-  Partial<Pick<IItem, "description" | "ruleName">>;
+export type ICreateItem = Omit<Item, "type" | "description" | "ruleName" | "data"> &
+  Partial<Pick<IItem, "description" | "ruleName" | "data">>;
 
 class Item implements IItem {
   public declare readonly type: ItemType;
   public name: string;
   public description: string | null;
   public ruleName: string | null;
+  public data: string | null;
 
-  constructor({ name, description, ruleName }: ICreateItem) {
+  constructor({ name, description, ruleName, data }: ICreateItem) {
     this.name = name;
     this.description = description ?? null;
     this.ruleName = ruleName ?? null;
+    this.data = data ?? null;
   }
 }
 
